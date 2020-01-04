72e2e918bc4d1196e818ffdc6d7fa5c27527c258
Benjamin Jones <ben@agiletea.com>
2020-01-04 15:11:19 +0000

    Added compelted code

--- FizzBuzz/program.py
+++ FizzBuzz/program.py
@@ -1,6 +1,15 @@
 # this is where your program's methods are define
 def printNumberRange(startNumber, endNumber):
     # start your code here
+    for index in range(startNumber, endNumber+1):
+        if index%15==0:
+            print('fizzbuzz', end=' ')
+        elif index%5==0:
+            print('buzz', end=' ')
+        elif index%3==0:
+            print('fizz', end=' ')
+        else:
+            print(index, end=' ')
     input("Program completed. Press any key to exit")
 
 # this is where the program will start!
