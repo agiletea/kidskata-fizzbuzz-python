1c3fa6b91825b3dce3474a0b76ce4788d2c4062f
Benjamin Jones <ben@agiletea.com>
2020-01-04 09:15:39 +0000

    Re-added Python folder back in

---
+++ Python/FizzBuzz/program.py
@@ -0,0 +1,13 @@
+# this is where your program's methods are define
+def printNumberRange(startNumber, endNumber):
+    # start your code here
+    input("Program completed. Press any key to exit")
+
+# this is where the program will start!
+print("Welcome to FizzBuzz!")
+
+x = 1
+y = 20
+
+printNumberRange(x, y)
+
